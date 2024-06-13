6883de57f9415f5d3ac2f93d43ac78c9ea7c1b6a
Lê Minh Việt Anh <hinhmomohinh@gmail.com>
2024-06-13 22:16:44 +0700

    ht

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ var Express = require("express");
 var Mongoclient = require("mongodb").MongoClient;
 var cors = require("cors");
 const { rateLimit } = require("express-rate-limit");
+// xac thuc
+const expressJwt = require('express-jwt');
+const jwt = require('jsonwebtoken');
 
 const apiLimiter = rateLimit({
     windowMs: 60 * 1000, // Thời gian cửa sổ (trong mili giây)
@@ -12,6 +15,7 @@ const apiLimiter = rateLimit({
 
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const { JsonWebTokenError } = require("jsonwebtoken");
 dotenv.config();
 const app = Express();
 
@@ -52,9 +56,43 @@ const memSchema = new Schema({
     phoneNumber: String,
     gender: {type: String, required: true},
     school: {type: String, required: true},
-    nation: String
+    nation: String,
+    username: {type: String, required: true, default: 'none'},
+    password: {type: String, required: true, default: 'none'},
+    role: {type: String, enum: ['admin', 'user'], default: 'user'}
 });
 const student = mongoose.model('vdter', memSchema);
+
+const generateToken = (user) => {
+    return jwt.sign({ id: user._id, role: user.role }, "123456789", { expiresIn: '12h' });
+};
+
+
+const authenticateToken = (req, res, next) => {
+    const token = req.cookies.token;
+    console.log("Token: ",token);
+    if (!token) {
+        return res.status(403).json({ message: 'No token provided' });
+    }
+    jwt.verify(token, "123456789", (err, user) => {
+        if (err) {
+            return res.status(403).json({ message: 'Failed to authenticate token' });
+        }
+        req.user = user;
+        next();
+    });
+};
+
+const authorizeRoles = (...roles) => {
+    return (req, res, next) => {
+        console.log("Roles: ",req.user.role);
+        if (!roles.includes(req.user.role)) {
+            return res.status(403).json({ message: 'Access denied', role: req.user.role, requiredRoles: roles});
+        }
+        next();
+    };
+};
+
 // Hien thi tat ca
 app.get("/all", async (req, res) => {
     try {
@@ -65,7 +103,7 @@ app.get("/all", async (req, res) => {
     }
 });
 // Them
-app.post("/add", async (req, res) => {
+app.post("/add", authenticateToken, authorizeRoles('admin', 'user'), async (req, res) => {
     try {
         const { name, age, email, phoneNumber, gender, school, nation } = req.body;
         const newStudent = await student.create({ name, age, email, phoneNumber, gender, school, nation });
@@ -78,7 +116,7 @@ app.post("/add", async (req, res) => {
     }
 })
 // Sua theo id
-app.put("/update/&id=:id", async (req, res) => {
+app.put("/update/&id=:id", authenticateToken, authorizeRoles('admin'), async (req, res) => {
     try {
         await student.findByIdAndUpdate(req.params.id, req.body);
         res.status(200).json(newUser);
@@ -87,7 +125,7 @@ app.put("/update/&id=:id", async (req, res) => {
     }
 });
 // Xoa theo id
-app.delete("/delete/&id=:id", async (req, res) => {
+app.delete("/delete/&id=:id", authenticateToken, authorizeRoles('admin'), async (req, res) => {
     try {
         const user = await student.findByIdAndDelete(req.params.id)
         res.status(200).json(user);
@@ -95,5 +133,31 @@ app.delete("/delete/&id=:id", async (req, res) => {
         res.status(400).json({ error: error.message })
     }
 })
+
+app.post("/check", async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const user = await student.findOne({ username });
+        if (!user || user.username === 'none' || user.password === 'none')  {
+            return res.status(401).json({ message: 'Invalid username' });
+        }
+        if (!compare(password, user.password)) {
+            return res.status(401).json({ message: 'Invalid password' });
+        }
+        console.log("touch");
+        const token = generateToken(user);
+        res.cookie('token', token, { httpOnly: true });
+        res.status(200).json({ message: `Logged in successfully as ${user.role}`, token, user });
+    } catch (error) {
+        logger.error({
+            message: error.message,
+            path: req.originalUrl,
+            method: req.method,
+            responseCode: 500
+        });
+        res.status(500).json({ message: error.message });
+    }
+});
+
 
 module.exports = app;
